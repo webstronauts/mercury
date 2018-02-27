2e3dff8dc84011697fa4a171a9bd2db59bbde0ac
Robin van der Vleuten <robin@webstronauts.co>
2018-02-27 17:01:37 +0100

    Added Next.js plugin

--- test/index.js
+++ test/index.js
@@ -1,11 +1,16 @@
+const cheerio = require('cheerio')
 const request = require('supertest')
 const test = require('ava')
 const mercury = require('../lib')
 
-test('mercury can be booted', async t => {
+function createMercuryInstance () {
+  return mercury({ logger: { level: 'silent' } })
+}
+
+test('Mercury can be booted', async t => {
   t.plan(2)
 
-  const app = mercury()
+  const app = createMercuryInstance()
 
   app.get('/', (req, res) => {
     res.send('Hello, world!')
@@ -16,4 +21,23 @@ test('mercury can be booted', async t => {
 
   t.is(res.status, 200)
   t.is(res.text, 'Hello, world!')
+})
+
+test('Next.js can be used to handle paths', async t => {
+  t.plan(2)
+
+  const app = createMercuryInstance()
+
+  app.after(err => {
+    if (err) throw err
+    app.get('*', app.next.getRequestHandler())
+  })
+
+  const res = await request(app)
+    .get('/')
+
+  const $ = cheerio.load(res.text)
+
+  t.is(res.status, 200)
+  t.is($('[data-reactroot]').text(), 'Hello, world!')
 })
