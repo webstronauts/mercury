993ef36e9a4e794c18105c8abc662cd485878743
Robin van der Vleuten <robin@webstronauts.co>
2018-03-04 15:44:18 +0100

    Transpile code before distributing (#8)
    * Lib files are transpiled before distributing
    
    * Code coverage is mapped to source files
    
    * Export default function for better interop
    
    * Run build scripts with correct NODE_ENV

---
+++ taskfile.js
@@ -0,0 +1,25 @@
+export async function build (task) {
+  await task.source('lib/**/*.js').babel({
+    presets: [
+      ['env', {
+        targets: { node: 'current' }
+      }]
+    ],
+    plugins: [
+      'transform-object-rest-spread',
+      'transform-runtime'
+    ],
+    env: {
+      test: {
+        plugins: [
+          'istanbul'
+        ],
+        sourceMaps: 'inline'
+      }
+    }
+  }).target('dist')
+}
+
+export default async function (task) {
+  await task.clear('dist').start('build')
+}
