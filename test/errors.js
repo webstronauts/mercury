3527def97f8fe17273adbb6d742d8167835d53e2
Robin van der Vleuten <robin@webstronauts.co>
2018-07-13 11:35:10 +0200

    Do not expose name of non-http errors. Closes #15

--- test/errors.js
+++ test/errors.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors')
+const { NotFound } = require('http-errors')
 const request = require('supertest')
 const test = require('ava')
 const mercury = require('../lib/index')
@@ -6,13 +6,13 @@ const mercury = require('../lib/index')
 test('Errors are serialized', async t => {
   t.plan(2)
 
-  const app = mercury({ logger: 'fatal' })
+  const app = mercury({ logger: 'silent' })
   app.load(mercury.errors)
 
   const message = 'This route cannot be found 🙈'
 
   app.get('/', (req, res) => {
-    throw new createError.NotFound(message)
+    throw new NotFound(message)
   })
 
   const res = await request(app)
@@ -20,4 +20,24 @@ test('Errors are serialized', async t => {
 
   t.is(res.status, 404)
   t.deepEqual(res.body, { error: { code: 'not_found', message } })
+})
+
+test('Error message is generic when it should not be exposed', async t => {
+  t.plan(2)
+
+  const app = mercury({ logger: 'silent' })
+  app.load(mercury.errors)
+
+  app.get('/', (req, res) => {
+    const err = new Error('This message should not be exposed')
+    err.name = 'FatalSecurityError'
+
+    throw err
+  })
+
+  const res = await request(app)
+    .get('/')
+
+  t.is(res.status, 500)
+  t.deepEqual(res.body, { error: { code: 'internal_server', message: 'An unexpected error occured' } })
 })
